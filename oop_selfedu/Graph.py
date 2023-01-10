908a55a4600d024c17a88cda808a3fc50f395d3f
Pavel <p.penkovskiy@gmail.com>
2023-01-10 21:09:40 +0300

    c5

---
+++ oop_selfedu/Graph.py
@@ -0,0 +1,21 @@
+# https://stepik.org/lesson/701974/step/6?unit=702075
+
+class Graph:
+    LIMIT_Y = [0, 10]
+    
+    def __init__(self):
+        self.data = None
+
+    def set_data(self, data):
+        self.data = data
+
+    def draw(self):
+        res = [i for i in self.data if Graph.LIMIT_Y[0] <= i <= Graph.LIMIT_Y[1]]
+        print(*res)
+
+graph_1 = Graph()
+
+
+graph_1.set_data([10, -5, 100, 20, 0, 80, 45, 2, 5, 7])
+
+graph_1.draw()
