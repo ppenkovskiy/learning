c9a524efebda3933500225252448dfe59008f3ec
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-04-06 12:13:13 +0300

    c23

--- oop_selfedu/Viber.py
+++ oop_selfedu/Viber.py
@@ -5,30 +5,26 @@ class Viber:
     def add_message(cls, msg):
         cls.msgs.append(msg)
 
-    def remove_message(self, msg):
-        self.msgs.remove(msg)
+    @classmethod
+    def remove_message(cls, msg):
+        cls.msgs.remove(msg)
 
-    def set_like(self, msg):
-        if self.msgs[self.msgs.index(msg)].fl_like:
-            self.msgs[self.msgs.index(msg)].fl_like = False
+    @classmethod
+    def set_like(cls, msg):
+        if cls.msgs[cls.msgs.index(msg)].fl_like:
+            cls.msgs[cls.msgs.index(msg)].fl_like = False
         else:
-            self.msgs[self.msgs.index(msg)].fl_like = True
+            cls.msgs[cls.msgs.index(msg)].fl_like = True
 
     def show_last_message(self, n):
         print(self.msgs[::-1][1:n:])
 
-    def total_messages(self):
-        return len(self.msgs)
+    @classmethod
+    def total_messages(cls):
+        return len(cls.msgs)
 
 
 class Message:
     def __init__(self, text, fl_like=False):
         self.text = text
         self.fl_like = fl_like
-
-
-msg_1 = Message("Hi")
-msg_2 = Message("Ho")
-Viber.add_message(msg_1)
-Viber.add_message(msg_2)
-print(len(Viber.msgs))
