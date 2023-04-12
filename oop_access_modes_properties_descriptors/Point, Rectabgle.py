37401e991007c3458fb9f56de062569d42459b7a
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-04-12 20:33:25 +0300

    c28

--- oop_access_modes_properties_descriptors/Point, Rectabgle.py
+++ oop_access_modes_properties_descriptors/Point, Rectabgle.py
@@ -8,14 +8,29 @@ class Point:
 
 
 class Rectangle:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+    def __init__(self, a, b, c=None, d=None):
+        if type(a) == Point:
+            self.a = a.get_coords()[0]
+            self.b = a.get_coords()[1]
+            self.c = b.get_coords()[0]
+            self.d = b.get_coords()[1]
+            self.__sp = Point(self.a, self.b)
+            self.__ep = Point(self.c, self.d)
+        else:
+            self.a = a
+            self.b = b
+            self.c = c
+            self.d = d
+            self.__sp = Point(self.a, self.b)
+            self.__ep = Point(self.c, self.d)
 
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
 
+    def get_coords(self):
+        return self.__sp, self.__ep
 
+    def draw(self):
+        print(f'Прямоугольник с координатами: ({self.a}, {self.b}) ({self.c}, {self.d})')
 
-
-
-
-pt = Point(x, y)
