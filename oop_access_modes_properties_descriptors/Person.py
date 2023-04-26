d6a6d95fb31e1a045e60c644eb8a849ed778bd68
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-04-26 20:36:17 +0300

    c32

---
+++ oop_access_modes_properties_descriptors/Person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, old):
+        self.__name = name
+        self.__old = old
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, old):
+        self.__old = old
+
+    @old.deleter
+    def old(self):
+        del self.__old
+
+
+p = Person('Pavel', 18)
+print(p.old)
+p.old = 20
+print(p.old)
+del p.old
+print(p.__dict__)
