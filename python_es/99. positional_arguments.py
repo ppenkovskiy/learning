8c5baac3016e3691c935b0845c11aea31244a0af
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-06-21 16:01:51 +0300

    c101

---
+++ python_es/99. positional_arguments.py
@@ -0,0 +1,29 @@
+def avg(a, b, c):
+    return (a + b + c) / 3
+
+
+print(avg(1, 2, 3))
+print(avg(a=1, b=2, c=3))
+
+
+def avg(a, b, c, /):
+    return (a + b + c) / 3
+
+
+print(avg(1, 2, 3))
+# print(avg(a=1, b=2, c=3)) # raise an exception
+
+
+def assert_equal(left, right, /, fail_messsage=''):  # before slash - positional only arguments
+    return (left == right, fail_messsage)
+
+
+print(assert_equal(1, 1))
+print(assert_equal(1, 2, fail_messsage='left not'))
+
+
+
+def copy(*, source, destination, overwrite=False):
+    print(f"Copying {source} to {destination} with overwrite = {overwrite}")
+
+    
