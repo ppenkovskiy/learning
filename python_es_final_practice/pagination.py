79477edacb458fc134ba5702b794eae1815d0a51
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-06-22 14:27:26 +0300

    c105

--- python_es_final_practice/pagination.py
+++ python_es_final_practice/pagination.py
@@ -3,51 +3,48 @@ import math
 
 class Pagination:
 
-    def __init__(self, items=[], page_size=10, current_page=1):
+    def __init__(self, items=[], page_size=10):
         self.items = items
         self.page_size = page_size
-        self.current_page = current_page
-        self.last_page_ = math.ceil(len(self.items) / self.page_size)
-
-    def get_visible_items(self):
-        return self.items[(self.current_page - 1) * self.page_size: self.current_page * self.page_size]
-
-    def prev_page(self):
-        if self.current_page > 1:
-            self.current_page -= 1
-        print(self.get_visible_items())
-
-    def next_page(self):
-        if self.current_page < self.last_page_:
-            self.current_page += 1
-        return self.get_visible_items()
-
-    def first_page(self):
+        self.total_pages = 1 if not self.items else math.ceil(len(self.items) / self.page_size)
         self.current_page = 1
-        return  self.get_visible_items()
-
-    def last_page(self):
-        self.current_page = self.last_page_
-        return self.get_visible_items()
-
-    def go_to_page(self, page):
-        if page < 1:
-            self.current_page = 1
-        elif page > self.last_page_:
-            self.current_page = self.last_page_
-        else:
-            self.current_page = page
-        return self.get_visible_items()
-
-    def get_current_page(self):
-        return self.current_page
-
-    def get_page_size(self):
-        return len(self.items[(self.current_page - 1) * self.page_size: self.current_page * self.page_size])
 
     def get_items(self):
         return self.items
 
+    def get_page_size(self):
+        return self.page_size
 
-p = Pagination(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 3)
-print(p.next_page().next_page())
+    def get_current_page(self):
+        return self.current_page
+
+    def prev_page(self):
+        if self.current_page > 1:
+            self.current_page -= 1
+        return self
+
+    def next_page(self):
+        if self.current_page < self.total_pages:
+            self.current_page += 1
+        return self
+
+    def first_page(self):
+        self.current_page = 1
+        return self
+
+    def last_page(self):
+        self.current_page = self.total_pages
+        return self
+
+    def go_to_page(self, page):
+        if page < 1:
+            page = 1
+        elif page > self.total_pages:
+            page = self.total_pages
+        self.current_page = page
+        return self
+
+    def get_visible_items(self):
+        start = (self.current_page - 1) * self.page_size
+        end = start + self.page_size
+        return self.items[start:end]
