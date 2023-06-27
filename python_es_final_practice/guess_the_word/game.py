63162fea908aec2e216d8aaad998818bbb60f0f4
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-06-27 16:46:00 +0300

    c108

---
+++ python_es_final_practice/guess_the_word/game.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(self, allowed_misses: int = 6):
+        if allowed_misses < 5 or allowed_misses > 8:
+            raise ValueError('Number of allowed misses should be between 5 and 8')
+        self.__allowed_misses = allowed_misses
+        self.__tries_counter = 0
+        self.__tried_letters = []
+        self.__open_indexes = []
+        self.__game_status = GameStatus.NOT_STARTED
+        
---
+++ python_es_final_practice/guess_the_word_first_try.py
@@ -0,0 +1,42 @@
+import random
+
+
+class GuessTheWord:
+
+    def __init__(self, mistakes=10):
+        with open('WordsStockRus.txt', mode='r', encoding='utf-8') as file:
+            words = file.readlines()
+            word = words[random.randint(0, len(words) - 1)].strip()
+        self.mistakes = mistakes
+        self.__word = list(word)
+        self.word = list(len(self.__word) * '*')
+        self.guessed_letters = []
+
+    def print_the_word(self):
+        print(''.join(self.word))
+
+    def guess_the_letter(self, letter):
+        self.guessed_letters.append(letter)
+        flag = True
+        for i in range(len(self.word)):
+            if letter == self.__word[i]:
+                self.word[i] = self.__word[i]
+                print(f'You guessed the letter "{letter}"')
+                flag = False
+        if flag:
+            self.mistakes -= 1
+            print(f'Rights to the mistake: {self.mistakes}')
+
+
+w = GuessTheWord()
+print(f"Your word: {''.join(w.word)}")
+
+while w.mistakes >= 1:
+    print("Guess the letter: ")
+    w.guess_the_letter(input())
+    print(''.join(w.word))
+
+if '*' not in w.word:
+    print('You win')
+else:
+    print('You lose')
