31afe37ad7a810f9c1306910af9660b34cb0c1b1
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-07-10 21:03:20 +0300

    230710

--- postgres_test/01_drop_create_table.sql
+++ postgres_test/01_drop_create_table.sql
@@ -1,16 +1,18 @@
 DROP TABLE IF EXISTS publisher;
 
-CREATE TABLE publisher(
+CREATE TABLE publisher
+(
     publisher_id integer PRIMARY KEY,
-    org_name varchar(128) NOT NULL,
-    address text
+    org_name     varchar(128) NOT NULL,
+    address      text
 );
 
 DROP TABLE IF EXISTS book;
 
-CREATE TABLE book (
+CREATE TABLE book
+(
     book_id integer PRIMARY KEY,
-    title text NOT NULL,
-    isbn varchar(32) NOT NULL
+    title   text        NOT NULL,
+    isbn    varchar(32) NOT NULL
 )
 
--- postgres_test/02_insert_data.sql
+++ postgres_test/02_insert_data.sql
@@ -1,7 +1,6 @@
 INSERT INTO book
-VALUES
-(1, 'Title_1', 121),
-(2, 'Title_2', 122),
-(3, 'Title_3', 123,
-(4, 'Title_4', 124),
-(5, 'Title_4', 124),
+VALUES (1, 'Title_1', 121),
+       (2, 'Title_2', 122),
+       (3, 'Title_3', 123),
+       (4, 'Title_4', 124),
+       (5, 'Title_4', 125)
