885a512bbf65e2ce31ae0dd73739fb6f62a613c8
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-07-18 16:42:54 +0300

    c132

--- postgres_test/24_homework.sql
+++ postgres_test/24_homework.sql
@@ -1,5 +1,54 @@
-select * from orders
+select *
+from orders
 where ship_country like 'U%';
 
-select
+select order_id, customer_id, freight, ship_country
 from orders
+where ship_country like 'N%'
+order by freight desc
+limit 10;
+
+select first_name, last_name, home_phone, region
+from employees
+where region is null;
+
+select count(*)
+from customers
+where region is not null;
+
+select country, count(*)
+from suppliers
+group by country
+order by count(*);
+
+SELECT ship_country, SUM(freight)
+FROM orders
+WHERE ship_region IS NOT NULL
+GROUP BY ship_country
+HAVING SUM(freight) > 2750
+ORDER BY sum(freight) DESC;
+
+select country
+from customers
+union -- union is working like distinct
+select country
+from suppliers
+order by country;
+
+select country
+from customers
+intersect
+select country
+from suppliers
+intersect
+select country
+from employees;
+
+select country
+from customers
+intersect
+select country
+from suppliers
+except
+select country
+from employees;
