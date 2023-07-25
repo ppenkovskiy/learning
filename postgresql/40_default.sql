44f66451b87fa7d7055562c3569aeb596fc6ad71
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-07-25 16:34:25 +0300

    c141

--- postgresql/40_default.sql
+++ postgresql/40_default.sql
@@ -3,7 +3,15 @@ drop table if exists customer cascade;
 create table customer
 (
     customer_id serial,
-    full_name text,
-    status char default 'regular'
+    full_name   text,
+    status      char default 'r',
+
+    constraint pk_customer_customer_id primary key (customer_id),
+    constraint chk_customer_status check (status = 'r' or status ='p')
 );
+
+insert into customer (full_name)
+values ('name');
+
+select * from customer;
 
