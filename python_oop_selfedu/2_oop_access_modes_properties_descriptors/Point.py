93a05032d48ee540133b3404467b62db8c364389
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-11-29 16:15:07 +0300

    153

--- python_oop_selfedu/2_oop_access_modes_properties_descriptors/Point.py
+++ python_oop_selfedu/2_oop_access_modes_properties_descriptors/Point.py
@@ -1,41 +1,31 @@
-# Class Point with protected attrs.
-# class Point_1:
-#     def __init__(self, x=0, y=0):
-#         self._x = x
-#         self._y = y
-#
-#
-# pt = Point_1(1, 2)
-# print(pt._x, pt._y)
-# pt._x = 3
-# pt._y = 4
-# print(pt._x, pt._y)
-
-
 # Class Point with private attrs.
-class Point_2:
-    def __init__(self, x=0, y=0):
-        self.__x = x
-        self.__y = y
+class Point:
 
-    def __check_value(self, x, y):
-        if type(x) not in (int, float) and type(y) not in (int, float):
-            raise ValueError('Coordinates should be numbers.')
+    @classmethod
+    def __check_value(cls, value):
+        return type(value) in (int, float)
+
+    def __init__(self, x=0, y=0):
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
         else:
-            pass
+            raise ValueError("Invalid coordinates")
 
     # setter
     def set_coord(self, x, y):
-        self.__check_value(x, y)
-        self.__x = x
-        self.__y = y
-        return (self.__x, self.__y)
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+            return (self.__x, self.__y)
+        else:
+            raise ValueError("Invalid coordinates")
 
     # getter
     def get_coord(self):
         return self.__x, self.__y
 
 
-pt = Point_2(1, 2)
+pt = Point(1, 2)
 pt.set_coord(3, 4)
 print(pt.get_coord())
