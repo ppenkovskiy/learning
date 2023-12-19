c83b72ecef73932ca5aeaeddadca3c6ae4286d96
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-12-19 16:59:49 +0300

    168

--- ya/6.py
+++ ya/6.py
@@ -1,8 +1,22 @@
-from collections import defaultdict
-
 with open('6_input.txt', 'r') as file:
     n = int(file.readline())
-    l = [i.split(' ') for i in file.readlines()]
+    l = [i.split(' ') for i in file if i[-1] != 'B' and i[-2:] != 'B\n']
+    l = [[int(item) if index < 4 else item for index, item in enumerate(sublist)] for sublist in l]
+    # print(l)
 
-l.sort(key=lambda x: x[-2])
-print(*l, sep='\n')
+rockets_id = sorted(set(int(i[-2]) for i in l))
+
+res = []
+for r_id in rockets_id:
+    res_r_id = 0
+
+    for i in range(len(l)):
+        if r_id == l[i][-2]:
+            if l[i][-1][0] == 'A':
+                res_r_id -= l[i][0] * 24 * 60 + l[i][1] * 60 + l[i][2]
+            elif l[i][-1][0] == 'C' or l[i][-1][0] == 'S':
+                res_r_id += l[i][0] * 24 * 60 + l[i][1] * 60 + l[i][2]
+
+    res.append(res_r_id)
+
+print(*res, sep=' ')
