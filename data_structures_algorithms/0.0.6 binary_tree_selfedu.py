0ead607ff23fbf32301041e5f1cb69ae51d56175
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-12-26 16:33:30 +0300

    175

---
+++ data_structures_algorithms/0.0.6 binary_tree_selfedu.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, data):
+        # creating list node
+        self.data = data
+        self.right = self.left = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def __find(self, node, parent, value):
+        if node is None:
+            return None, parent, False
+
+        if value == node.data:
+            # 'True' means the required value is found
+            return node, parent, True
+
+        if value < node.data:
+            if node.left:
+                return self.__find(node.left, node, value)
+
+        if value > node.data:
+            if node.right:
+                return self.__find(node.right, node, value)
+
+        return node, parent, False
+
+    def append(self, obj):
+        if self.root is None:
+            self.root = obj
+            return obj
+
+        s, p, fl_find = self.__find(self.root, None, obj.data)
+
+        if not fl_find and s:
+            if obj.data < s.data:
+                s.left = obj
+            else:
+                s.right = obj
+
+        return obj
+
+    def show_tree(self, node):
+        if node is None:
+            return
+
+        self.show_tree(node.left)
+        print(node.data)
+        self.show_tree(node.right)
+
+
+v = [10, 5, 7, 16, 13, 2, 20]
+
+t = Tree()
+for x in v:
+    t.append(Node(x))
+
+t.show_tree(t.root)
