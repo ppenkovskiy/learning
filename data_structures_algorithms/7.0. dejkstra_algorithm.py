ca2fbfeb5a11a1bc8fe1b0524f7476518e20fe55
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-12-28 11:02:47 +0300

    180

---
+++ data_structures_algorithms/7.0. dejkstra_algorithm.py
@@ -0,0 +1,62 @@
+graph = {}
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
+
+# print(type(graph['start']))
+# print(graph['start'].keys())
+# print(graph['start']['a'])
+# print(graph['start']['b'])
+
+graph['a'] = {}
+graph['a']['fin'] = 1
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+graph['fin'] = {}  # end node has no neighbors
+
+infinity = float('inf')
+
+# the cost of a node determines how long it will
+# take to get to that node from the STARTING node
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
+# each node should be processed only once
+processed_nodes = []
+
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed_nodes:
+            lowest_cost = cost
+            lowest_cost_node = node
+
+    return lowest_cost_node
+
+
+node = find_lowest_cost_node(costs)
+
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+
+    processed_nodes.append(node)
+    node = find_lowest_cost_node(costs)
