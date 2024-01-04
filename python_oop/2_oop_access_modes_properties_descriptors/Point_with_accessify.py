8c161672bac250afeec0d637ec9502d965f04f07
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-01-04 13:05:46 +0300

    1941

--- python_oop/2_oop_access_modes_properties_descriptors/Point_with_accessify.py
+++ python_oop/2_oop_access_modes_properties_descriptors/Point_with_accessify.py
@@ -1,19 +1,22 @@
-# Class Point with private attrs.
+from accessify import private, protected
+
+
 class Point:
     def __init__(self, x=0, y=0):
-        if self.__check_value(x) and self.__check_value(y):
+        if self.check_value(x) and self.check_value(y):
             self.__x = x
             self.__y = y
         else:
             raise ValueError("Invalid coordinates")
 
+    @private
     @classmethod
-    def __check_value(cls, value):
+    def check_value(cls, value):
         return type(value) in (int, float)
 
     # setter (interface methods)
     def set_coord(self, x, y):
-        if self.__check_value(x) and self.__check_value(y):
+        if self.check_value(x) and self.check_value(y):
             self.__x = x
             self.__y = y
             return (self.__x, self.__y)
@@ -27,4 +30,4 @@ class Point:
 
 pt = Point(1, 2)
 pt.set_coord(3, 4)
-print(dir(pt))
+pt.check_value(5)
