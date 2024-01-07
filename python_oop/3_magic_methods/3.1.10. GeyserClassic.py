f0628bbb59518bd0867280131e4d96f01e80774d
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-01-07 20:42:22 +0300

    213

---
+++ python_oop/3_magic_methods/3.1.10. GeyserClassic.py
@@ -0,0 +1,32 @@
+class GeyserClassic:
+    MAX_DATE_FILTER = 100
+
+    def add_filter(self, slot_num, filter):
+
+
+
+class Mechanical:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date':
+            return
+
+
+class Aragon:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date':
+            return
+
+
+class Calcium:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date':
+            return
