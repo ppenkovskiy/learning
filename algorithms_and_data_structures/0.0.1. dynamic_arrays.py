12bfd9e30194e450e213615b850a6006f77bd19e
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-03-21 21:18:55 +0300

    237

--- algorithms_and_data_structures/0.0.1. dynamic_arrays.py
+++ algorithms_and_data_structures/0.0.1. dynamic_arrays.py
@@ -1,10 +1,10 @@
 # lists in Python are implemented as dynamic arrays of references
 marks = [2, 2, 3, 4]
-lst = [True, 'True', 1, 1.0, 5]
-
-lst.insert(0, 'First')  # O(n)
-
 marks[0] = 3  # O(1)
+
+lst = [True, 'True', 1, 1.0, 5]
+lst.append(6)  # O(1)
+lst.insert(0, 'First')  # O(n)
 
 n = len(marks)
 m = len(marks)
@@ -12,3 +12,5 @@ marks_plus_lst = marks + lst  # O(n+m)
 
 lst_1 = [1, 2, 3, 4, 5]
 lst_2 = lst_1[1:4]  # O(n)
+
+
