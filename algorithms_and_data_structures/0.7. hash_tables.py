edfde971621bf8f9decb862f013b19b17f7f57a8
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-03-26 12:30:24 +0300

    240

---
+++ algorithms_and_data_structures/0.7. hash_tables.py
@@ -0,0 +1,10 @@
+# PYTHON DICTIONARIES AND SETS ARE IMPLEMENTED BASED ON HASH-TABLES.
+
+# DICTIONARY EXAMPLE
+d = {'one': 1, 'two': 2, 'three': 3}
+value = d['one']
+print(value)
+
+# SET EXAMPLE
+d = {1, 2, 3}
+print(d)
